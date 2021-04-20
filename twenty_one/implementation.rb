6b2db5dfc033f5f512c478059d4180b51db90a1c
carolinaavila@protonmail.com <@Ovidio108>
2021-04-19 20:36:19 -0400

    Twenty One game

--- twenty_one/implementation.rb
+++ twenty_one/implementation.rb
@@ -1,6 +1,253 @@
-
+class Card
   SUITS = %w(H D S C)
   FACES = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 
-  p SUITS
-  p FACES
+  def initialize(suit, face)
+    @suit = suit
+    @face = face
+  end
+
+  def to_s
+    "The #{face} of #{suit}"
+  end
+
+  def face
+    case @face
+    when "J" then "Jack"
+    when "Q" then "Queen"
+    when "K" then "King"
+    when "A" then "Ace"
+    else
+      @face
+    end
+  end
+
+  def suit
+    case @suit
+    when "H" then "Hearts"
+    when "D" then "Diamonds"
+    when "S" then "Spades"
+    when "C" then "Clubs"
+    end
+  end
+
+  def ace?
+    face == 'Ace'
+  end
+
+  def king?
+    face == 'King'
+  end
+
+  def queen?
+    face == 'Queen'
+  end
+
+  def jack?
+    face == 'Jack'
+  end
+end
+
+class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    Card::SUITS.each do |suit|
+      Card::FACES.each do |face|
+        @cards << Card.new(suit, face)
+      end
+    end
+
+    scramble!
+  end
+
+  def scramble!
+    cards.shuffle!
+  end
+
+  def deal_one
+    cards.pop
+  end
+end
+
+module Hand
+  def show_hand
+    puts "---- #{name}'s Hand ----"
+    cards.each do |card|
+      puts "=> #{card}"
+    end
+    puts "=> Total: #{total}"
+    puts ""
+  end
+
+  def total
+    total = 0
+     cards.each do |card|
+      if card.ace?
+        total += 11
+      elsif card.jack? || card.queen? || card.king?
+        total += 10
+      else
+        total += card.face.to_i
+      end
+    end
+
+    # correct for Aces
+    cards.select(&:ace?).count.times.do
+    break if total <= 21
+    total -= 10
+  end
+
+  total
+end
+
+  def add_card(new_card)
+    cards << new_card
+  end
+
+  def busted?
+    total > 21
+  end
+end
+
+class Participant
+  include Hand
+
+  attr_accessor :name, :cards
+  def initialize
+    @cards = []
+    set_name
+  end
+end
+
+class Player < Participant
+  def set_name
+    name = ''
+    loop do
+      puts "What's your name?"
+      name = gets.chomp
+      break unless name.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = name
+  end
+
+  def show_flop
+    show_hand
+  end
+end
+
+class Dealer < Participant
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
